20dc9aa10c0ee9ecf6e6c2d94edb42553a6420ae
anjanamp35 <84272270+anjanamp35@users.noreply.github.com>
2022-07-16 15:42:22 +0530

    Add files via upload

---
+++ oddeven.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+ int n;
+ printf("Enter the number:");
+ scanf("%d",&n);
+  if(n%2==0)
+  {
+   printf("if no is even");
+  }
+  else
+  {
+   printf("if no is odd");
+  }
+ return 0;
+}
