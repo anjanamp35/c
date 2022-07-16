8a3443fcb136407872a9f3b8de6b2132ec0a255a
anjanamp35 <84272270+anjanamp35@users.noreply.github.com>
2022-07-16 15:48:21 +0530

    Add files via upload

---
+++ twogr.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{ 
+ int a,b;
+ printf("Enter two numbers:");
+ scanf("%d%d",&a,&b);
+ if(a>b)
+  {
+   printf("if a is greater than b is %d",a);
+  }
+ else
+  {
+   printf("if b is grater than a is %d",b);
+  }
+ return 0;
+}
